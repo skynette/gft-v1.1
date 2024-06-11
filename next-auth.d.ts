6906ee115a55f648968a8f4f87d6c93354f35e6e
joshua hassan <cutejosh2@gmail.com>
2024-06-11 08:19:23 +0100

    update next auth types

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,12 +1,35 @@
-// next-auth.d.ts
-import NextAuth from "next-auth";
+import NextAuth, { Profile as NextAuthProfile } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     accessToken?: string;
+    user: {
+      id?: string;
+      firstName?: string;
+      lastName?: string;
+      email: string;
+      name: string;
+      image: string;
+      picture?: string;
+    };
   }
 
   interface User {
     token?: string;
+  }
+
+  interface JWT {
+    accessToken?: string;
+    id?: string;
+    firstName?: string;
+    lastName?: string;
+    picture?: string;
+  }
+
+  interface Profile extends NextAuthProfile {
+    given_name?: string;
+    family_name?: string;
+    picture?: string;
+    sub?: string;
   }
 }
