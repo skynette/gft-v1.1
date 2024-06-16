a841ffc704f2fb9ff2849aaaae56fe6eb5f5ede8
joshua hassan <cutejosh2@gmail.com>
2024-06-16 11:38:39 +0100

    change default contact preference to email

--- server/apps/authentication/models.py
+++ server/apps/authentication/models.py
@@ -31,7 +31,7 @@ class User(TimeStampedUUIDModel, AbstractUser):
     username = models.TextField(max_length=100, unique=True)
     email = models.EmailField(null=True, blank=True, unique=True)
     mobile = PhoneNumberField(verbose_name=_("Phone number"), max_length=20, default="+23491234567")
-    contact_preference = models.CharField(max_length=100,choices=CONTACT_PREFERENCE_CHOICES,default="phone")
+    contact_preference = models.CharField(max_length=100,choices=CONTACT_PREFERENCE_CHOICES,default="email")
     image = models.ImageField(upload_to=custom_upload_to, null=True, blank=True)
     provider = models.CharField(max_length=100, choices=PROVIDER_CHOICES, default="credentials")
     user_type = models.CharField(
