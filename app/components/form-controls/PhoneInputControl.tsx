12ed3362a955633fc5b9b0eb384969b08b1ad6ae
skynette <cutejosh2@gmail.com>
2024-06-25 13:39:06 +0100

    chore: Add dep for react phone number input

---
+++ app/components/form-controls/PhoneInputControl.tsx
@@ -0,0 +1,33 @@
+import { ErrorMessage, Field, FieldProps } from 'formik';
+import { FC } from 'react';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+import { PhoneInput } from '../ui/phone-input';
+
+interface PhoneInputProps {
+    name: string;
+    label: string;
+    type: string;
+    placeholder: string;
+}
+
+const PhoneInputControl: FC<PhoneInputProps> = ({ name, label, type, placeholder }) => {
+    return (
+        <div>
+            <Field name={name}>
+                {
+                    (props: FieldProps) => (
+                        <div className='flex flex-col space-y-2'>
+                            <Label htmlFor={name} className='font-normal text-sm'>{label}</Label>
+                            <PhoneInput type={type} className='rounded-lg w-full text-sm bg-white'
+                                id={name} placeholder={placeholder} {...props.field} />
+                        </div>
+                    )
+                }
+            </Field>
+            <ErrorMessage name={name} render={msg => <small className='text-red-500 text-xs'>{msg}</small>} />
+        </div>
+    )
+}
+
+export default PhoneInputControl;
