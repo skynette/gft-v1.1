d8a53633e4a132c953f695215c58e7a246afef48
joshua hassan <cutejosh2@gmail.com>
2024-06-25 16:44:23 +0100

    ts-ignore for phone input

--- app/components/ui/phone-input.tsx
+++ app/components/ui/phone-input.tsx
@@ -52,6 +52,7 @@ const PhoneInput: React.ForwardRefExoticComponent<PhoneInputProps> =
                      *
                      * @param {E164Number | undefined} value - The entered value
                      */
+                    // @ts-ignore
                     onChange={(value) => onChange?.(value || "")}
                     {...props}
                 />
