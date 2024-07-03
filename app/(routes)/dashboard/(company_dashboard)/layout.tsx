4148ec32081c6ccf1ed68264ccddf22c463565fc
joshua hassan <cutejosh2@gmail.com>
2024-07-03 11:04:35 +0100

    company settings layout

--- app/(routes)/dashboard/(company_dashboard)/layout.tsx
+++ app/(routes)/dashboard/(company_dashboard)/layout.tsx
@@ -15,9 +15,9 @@ export default function DashboardLayout({
   return (
     <>
       <Header />
-      <div className="flex h-screen overflow-hidden">
+      <div className="flex">
         <Sidebar />
-        <main className="flex-1 overflow-hidden">{children}</main>
+        <main className="flex-1">{children}</main>
       </div>
     </>
   );
