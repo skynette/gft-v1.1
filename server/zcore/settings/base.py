937695345f3e82d049dec085ead041484e8a1599
joshua hassan <cutejosh2@gmail.com>
2024-07-10 16:02:42 +0100

    redeploy api

--- server/zcore/settings/base.py
+++ server/zcore/settings/base.py
@@ -129,7 +129,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images) 
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
